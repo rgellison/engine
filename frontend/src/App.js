94cf8bab790f92a0f7a6c2d2f9082440d934020e
rgellison <rgellison@live.com>
2022-02-07 21:28:43 +0000

    first commit - 07/02

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">engine</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
