065e34a6c66a0fbf5e937ded0315104bea377a33
rgellison <rgellison@live.com>
2022-02-21 10:23:13 +0000

    21.02.2022

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 1. Install Tools
 2. Create React App
 3. Create Git Repository
+4. Created main feature screen with temproray data
+5. Created singular product screen with tenporary data
+6. updated URLs
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
